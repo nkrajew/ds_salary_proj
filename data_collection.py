3dc1a5ed87d70dfdf90ff19c3b06b8e238682b2f
nkrajew <nkrajew@umich.edu>
2020-09-23 18:22:58 -0400

    uploaded scraper

---
+++ data_collection.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 16:23:17 2020
+
+@author: nkraj
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "C:/Users/nkraj/Documents/Data Science/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs('data scientist', 1000, False, path, 5)
+
